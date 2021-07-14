6aae1aba1ee7e96a355bf04206b27dae7aecafeb
A-Siam <abdullah.khaled.kamal0@gmail.com>
2021-07-14 12:22:48 +0200

    add explicit eureka client annotation

--- src/main/java/io/asiam/tansiq/TansiqApplication.java
+++ src/main/java/io/asiam/tansiq/TansiqApplication.java
@@ -3,9 +3,11 @@ package io.asiam.tansiq;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TansiqApplication {
 
 	public static void main(String[] args) {
